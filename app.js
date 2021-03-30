cdf02a57535780361dad421f9129426b43da04ec
Daniel Ackerman <drackerm@uci.edu>
2021-03-30 17:51:55 -0500

    Set up momgodb, passport and sesion

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const User = require('./models/user');
+const session = require('express-session');
+const mongoose = require('mongoose');
 
+
+// require routes
 const index   = require('./routes/index');
 const posts   = require('./routes/posts');
 const reviews = require('./routes/reviews');
 
 const app = express();
+
+// connect to db
+mongoose.connect('mongodb://localhost:27017/surf-shop', {
+  useNewUrlParser: true,  
+  useUnifiedTopology: true, 
+  useCreateIndex: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'conncetion error:'));
+db.once('open', () => {
+  console.log('Mongo DB Connected!');
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,6 +38,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Config passport and session
+app.use(session({
+  secret: 'local session secret',
+  resave: false,
+  saveUninitialized: true
+}));
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); 
+
+// Mount routes
 app.use('/', index);
 app.use('/posts', posts);
 app.use('/posts/:id/reviews', reviews);
